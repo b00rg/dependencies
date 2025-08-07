ce3290e1a22c20d46553d59590393013b380d5a2
Emma <emma.grace.burgess@gmail.com>
2025-08-07 17:53:05 +0100

    basic working scanner

---
+++ dependencies/scanner.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+with open("generated_requirements.txt", "w") as requirements:
+    cumulative_reqs = set()
+
+    for path in Path("./tests/test-repo/.").rglob("*.py"):
+        with open(path, "r", encoding="utf-8") as file:
+            for line in file:
+                reqs = []
+                line = line.strip()
+
+                if line.startswith("from "):
+                    reqs = line.removeprefix("from ").split(" import ")[0]
+                    reqs = [r.strip() for r in reqs.split(",")] # for modules like sklearn.cluster, we just want sklearn
+
+                elif line.startswith("import "):  # import x,y,z vs. import x
+                    reqs = line.removeprefix("import ")
+                    reqs = reqs.split(",")
+                    
+                if reqs:
+                    for req in reqs:
+                        if req not in cumulative_reqs:
+                            requirements.write(req + "\n")
+                            cumulative_reqs.add(req)
