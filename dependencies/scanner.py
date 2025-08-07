5e7e5580112e217b4cad8dba4e93419b1fd3bd0e
Emma <emma.grace.burgess@gmail.com>
2025-08-07 18:15:44 +0100

    removing base python versions

--- dependencies/scanner.py
+++ dependencies/scanner.py
@@ -1,7 +1,12 @@
 from pathlib import Path
+from stdlib_list import stdlib_list
+import platform
 
 with open("generated_requirements.txt", "w") as requirements:
     seen_reqs = set()
+    python_version = platform.python_version().split(".")
+    python_version = ".".join(python_version[:2])
+    stdlib = set(stdlib_list(python_version))
     for path in Path("./tests/test-repo/.").rglob("*.py"):
         with open(path, "r", encoding="utf-8") as file:
             for line in file:
@@ -19,6 +24,6 @@ with open("generated_requirements.txt", "w") as requirements:
                 if reqs:
                     for req in reqs:
                         base_req = req.split(".")[0]
-                        if base_req not in seen_reqs:
+                        if base_req not in seen_reqs and base_req not in stdlib:
                             requirements.write(base_req + "\n")
                             seen_reqs.add(base_req)
